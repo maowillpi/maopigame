86c1695862edc705fc6edb97f6616d2c8543c860
maowillpi <maowillpi@example.com>
2025-07-07 22:24:10 +0800

    重大升级：完全优化的移动端游戏界面

---
+++ _next/static/chunks/pages/mobile-efcac9be3ccfd4c7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[422],{9644:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/mobile",function(){return u(2309)}])}},function(n){n.O(0,[687,309,774,888,179],function(){return n(n.s=9644)}),_N_E=n.O()}]);
